85e54fc749cfe557da5925a3ed137c3e7550bca5
wengervictor329@gmail.com <wengervictor329@gmail.com>
2021-01-25 14:10:26 +0100

    fin partie 2

---
+++ Partie2/request.sql
@@ -0,0 +1,27 @@
+--exo1
+USE webDevelopment;
+CREATE TABLE languages(id INT, language VARCHAR(255));
+--exo2
+USE webDevelopment;
+CREATE TABLE tools(id INT, tool VARCHAR(255));
+--exo3
+USE webDevelopment;
+CREATE TABLE IF NOT EXISTS frameworks(id INT, name VARCHAR(255));
+--exo4
+USE webDevelopment;
+CREATE TABLE libraries(id INT, library VARCHAR(255));
+--exo5
+USE webDevelopment;
+CREATE TABLE ide (id INT, ideName VARCHAR(255));
+--exo6
+USE webDevelopment;
+CREATE TABLE IF NOT EXISTS frameworks (id INT, name VARCHAR(255));
+--exo7
+DROP TABLE IF NOT EXISTS tools;
+--exo8
+DROP TABLE libraries;
+--exo9
+DROP TABLE ide;
+--tp
+
+
