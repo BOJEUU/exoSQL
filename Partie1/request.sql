09c01e36d1e19ca5c9b984c74a7e400060399cf4
wengervictor329@gmail.com <wengervictor329@gmail.com>
2021-01-25 11:24:40 +0100

    fin partie 1

---
+++ Partie1/request.sql
@@ -0,0 +1,15 @@
+--exo1
+CREATE DATABASE languages;
+--exo2
+CREATE DATABASE webDevelopment CHARACTER SET utf8 COLLATE utf8_general_ci;
+--exo3
+CREATE DATABASE IF NOT EXISTS frameworks CHARACTER SET utf8 COLLATE utf8_general_ci;
+--exo4
+CREATE DATABASE IF NOT EXISTS languages CHARACTER SET utf8 COLLATE utf8_general_ci;
+--exo5
+DROP DATABASE languages;
+--exo6
+DROP DATABASE IF NOT EXISTS frameworks;
+--exo7
+DROP DATABASE IF NOT EXISTS languages;
+
