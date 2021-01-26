88904f3817a5e1503e878c1ba424a9fe5ece43db
wengervictor329@gmail.com <wengervictor329@gmail.com>
2021-01-26 15:52:37 +0100

    fin partie 6 et 7

---
+++ Partie6/request.sql
@@ -0,0 +1,6 @@
+--exo1
+SELECT * FROM webdevelopment.frameworks WHERE version > "version 2"; 
+--exo2
+SELECT * FROM webDevelopment.frameworks WHERE id IN (1,3);
+--exo3
+SELECT * FROM webdevelopment.ide WHERE date BETWEEN "2010-01-01" AND "2011-12-31";
---
+++ Partie7/request.sql
@@ -0,0 +1,6 @@
+--exo1
+DELETE FROM webDevelopment.languages WHERE framework = "HTML";
+--exo2
+UPDATe webDevelopment.frameworks SET framework = "Symfony2" WHERE framework = "Symfony";
+--exo3
+UPDATe webDevelopment.frameworks SET framework = "version 5.1" WHERE framework ="Javascript" AND version = "version 5";
